25a23e29773ce500f6a77b8ca1a2983598c69ec3
fakhriFauzi <fakhri.fauzi14@gmail.com>
2015-10-13 19:37:34 +0700

    versi1

---
+++ src/newpackage/NewClass.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author sahabat
+ */
+public class NewClass {
+      public int nilaiHasil;
+    public float nilaiHasil2;
+    int perjumlahan (int a, int b) {
+        nilaiHasil = a + b;
+        return nilaiHasil;
+        
+}
+       int pengurangan (int a, int b) {
+        nilaiHasil = a - b;
+        return nilaiHasil;
+}
+         int perkalian (int a, int b) {
+        nilaiHasil = a * b;
+        return nilaiHasil;
+}
+             float pembagian (float a, float b) {
+        nilaiHasil2 = a / b;
+      return nilaiHasil2;
+}
+}
